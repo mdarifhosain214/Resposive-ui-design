f2eb743121f66fe60f10774c6c1a52cfd3089eae
Hosain Arif <mdarifhosain214@gmail.com>
2023-10-04 12:23:07 +0600

    layout Builder file location change.

---
+++ lib/layout_builder.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+class LayoutBuilderClass extends StatelessWidget {
+  const LayoutBuilderClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double h =(MediaQuery.sizeOf(context).height) ;
+    double w =(MediaQuery.sizeOf(context).width) ;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title:const  Text("Layout builder widget"),
+      ),
+      body: LayoutBuilder(builder: (BuildContext context,BoxConstraints constraints){
+        if(constraints.maxWidth>600){
+          return _buildLandscape(h/2);
+        }
+        else{
+          return _buildPortraid(w/2);
+        }
+      }),
+    );
+
+  }
+  Widget _buildPortraid(double size){
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.teal,
+            height:size,
+            width: size,
+          ),
+          Container(
+            color: Colors.teal,
+            height:size,
+            width: size,
+          ),
+          Container(
+            color: Colors.teal,
+            height:size,
+            width: size,
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _buildLandscape(double size){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          color: Colors.teal,
+          height:size,
+          width: size,
+        ),
+        Container(
+          color: Colors.teal,
+          height:size,
+          width: size,
+        ),
+        Container(
+          color: Colors.teal,
+          height:size,
+          width: size,
+        ),
+
+
+      ],);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-import 'dart:js';
 
 import 'package:flutter/material.dart';
+
+import 'layout_builder.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,64 +15,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Layout builder widget',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LayoutBuilderClass(),
     );
   }
 }
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    double size =(MediaQuery.sizeOf(context).width/3) ;
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title:const  Text("Layout builder widget"),
-      ),
-      body: LayoutBuilder(builder: (BuildContext context,BoxConstraints constraints){
-        if(constraints.maxWidth>600){
-          return _buildLandscape(size);
-        }
-        else{
-          return _buildPortraid(size);
-        }
-      }),
-    );
-
-  }
-  Widget _buildPortraid(double size){
-    return  Center(
-      child: Container(
-        color: Colors.teal,
-        height:size,
-        width: size,
-      ),
-    );
-  }
-  Widget _buildLandscape(double size){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Container(
-          color: Colors.teal,
-          height:size,
-          width: size,
-        ),
-        Container(
-          color: Colors.teal,
-          height:size,
-          width: size,
-        ),
-
-
-    ],);
-  }
-}
 
 
 
